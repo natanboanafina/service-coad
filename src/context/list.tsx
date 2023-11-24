a3bbe61f15d3282415a9a6fd7fc1feec9e832514
natanboanafina <boanafina.natan@gmail.com>
2023-11-24 18:21:50 -0300

    chore: code explanation added

--- src/context/list.tsx
+++ src/context/list.tsx
@@ -1,25 +1,29 @@
 import { createContext, ReactNode, useState, useEffect } from "react";
 
+// Interface com os tipos necessários para o contexto.
 interface FormDataContextProps {
   formData: any[];
   updateFormData: (data: any) => void;
 }
 
+// Contexto criado com estado inicial.
 const FormDataContext = createContext<FormDataContextProps | undefined>(
   undefined
 );
 
+// Provider criado para poder passar os dados para os componentes filhos.
 export const FormDataProvider = ({ children }: { children: ReactNode }) => {
   const [formData, setFormData] = useState<any[]>([]);
 
   useEffect(() => {
-    // Recupere os dados do localStorage ao carregar a página
+    // Recupera os dados do localStorage ao carregar a página
     const storedFormData = localStorage.getItem("formData");
     if (storedFormData) {
       setFormData(JSON.parse(storedFormData));
     }
   }, []);
 
+  // Atualiza os dados do localStorage toda vez que o estado formData for atualizado.
   const updateFormData = (data: any) => {
     setFormData((prevData) => {
       const newData = [...prevData, data];
