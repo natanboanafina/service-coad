f9a04217afbbda5cdc4173902fc1e5e400561070
natanboanafina <boanafina.natan@gmail.com>
2023-11-24 18:19:05 -0300

    chore: code explanation added

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,5 +1,9 @@
 import * as Styled from "./styles";
 
+// as = propriedade que permite escolher qual tag HTML o componente será. No caso abaixo, ela pode ser
+// h1, h2, h3, p ou span.
+// $weight = propriedade que permite escolher o peso da fonte. No caso abaixo, ela pode ser bold,
+// medium ou normal. Ela também leva esse cifrão indicando ser uma propriedade read-only. Em suma, ela não modifica o DOM.
 interface ITextProps {
   content: string;
   style?: React.CSSProperties;
