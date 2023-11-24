71aa220abab14819d4f1dc7cf8a9864b7e049178
natanboanafina <boanafina.natan@gmail.com>
2023-11-24 18:24:17 -0300

    chore: code explanation added

--- src/context/useFormData.tsx
+++ src/context/useFormData.tsx
@@ -1,8 +1,11 @@
 import { useContext } from "react";
 import { FormDataContext } from "./list";
 
+// hook criado para recuperar informações do context e usá-lo em qualquer parte do projeto,
+// que esteja dentro de seu provider.
 const useFormData = () => {
   const context = useContext(FormDataContext);
+  // Uma validação para caso o hook seja utilizado fora do provider.
   if (!context) {
     throw new Error("useFormData must be used within a FormDataProvider");
   }
